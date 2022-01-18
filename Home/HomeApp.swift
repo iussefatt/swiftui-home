a2a1c7f06268cc0216552dec67abc9ba2534cff2
Ismael Ussefatt <ismael.ussefatt@xpi.com.br>
2022-01-18 16:37:07 -0300

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by Ismael Ussefatt on 18/01/22.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
